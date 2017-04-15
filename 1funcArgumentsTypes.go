bfc9d7e962acc235bd3c444520219e37e5179d19
Andrew Koidan <deathangel908@gmail.com>
2017-04-15 23:53:24 +0300

    starting learning

---
+++ 1funcArgumentsTypes.go
@@ -0,0 +1,34 @@
+package main
+
+// difference := = var
+import (
+//"math/cmplx"
+)
+import "fmt"
+
+var java, python bool
+
+const (
+	a = 1 << 100
+	b = a >> 97
+)
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
+func add(x, y int) (sum int, difference int) {
+
+	//println(i)
+	sum = x + y
+	difference = x - y
+	return
+}
+
+func main() {
+	//var i = 344333333333333333333333333333333333.3
+	fmt.Println(needInt(b))
+	fmt.Println(needFloat(b))
+	fmt.Println(a)
+}
---
+++ 2forIfDefferSwitch.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"runtime"
+	"time"
+)
+
+func forever(end int) int {
+	i := 1
+	for {
+		if i > end {
+			break
+		}
+		i += i
+	}
+	return i
+}
+
+func while(end int) (i int) {
+	i = 1
+	for i < end {
+		i += i
+	}
+	return
+}
+
+func fullFor(end int) (j int) {
+	for j = 1; j < end; j += j {
+	}
+	return j
+}
+
+func ifStatement(end float64) {
+	if d := math.Pow(end, 2); d < 10 {
+		print("It works")
+	} else {
+		print("It doesnt work")
+	}
+}
+
+func sqrt(number int, precision int) float64  {
+	z := 1.0
+	for i := 1; i < precision; i++ {
+		z = z - (z*z-float64(number))/(2*z)
+	}
+	return z
+}
+
+func isSaturdaySoon(){
+	nowDay := time.Now().Weekday()
+	diff := time.Saturday - nowDay
+	switch {
+	case diff < 2:
+		print("good")
+	case diff < 4:
+		print("bad")
+	default:
+		print("Holly fuck :(")
+	}
+	
+}
+
+func switchStatement()  {
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		print("OS X")
+	case "linux":
+		print("Linux")
+	default:
+		print(os)
+	}
+}
+
+func getNumber(arg int) int {
+	fmt.Printf("Getting number %v\n", arg)
+	return arg
+}
+
+func deferStatement(number int)  {
+	fmt.Println("Preparing deffer")
+	defer fmt.Printf("Deffer %v is called", getNumber(number))
+	fmt.Println("Deffer is used")
+}
+
+func stackDeffer() {
+	for i := 0; i< 4; i++ {
+		defer deferStatement(i)
+	}
+	fmt.Println("All deffers are stacked")
+
+}
+func main() {
+	fmt.Println("Starting program")
+	//fmt.Println(fullFor(100))
+	//fmt.Println(while(100))
+	//fmt.Println(forever(100))
+	//ifStatement(2.0)
+	//fmt.Printf("Newton's  %v, Math: %v", sqrt(4,4), math.Sqrt(4))
+	//switchStatement()
+
+	//isSaturdaySoon()
+	stackDeffer()
+}

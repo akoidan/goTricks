c30c454f6a24ce7990959e071a387d230117c5e2
Andrew Koidan <deathangel908@gmail.com>
2017-04-18 10:11:51 +0300

    learning methods

--- 4MethodsAndInterfaces.go
+++ 4MethodsAndInterfaces.go
@@ -1,5 +1,6 @@
 package main
-//TODO https://tour.golang.org/methods/2
+
+//TODO https://tour.golang.org/methods/6
 import "fmt"
 
 type XYVertex struct {
@@ -7,11 +8,43 @@ type XYVertex struct {
 	y int
 }
 
+type lol float64
+
+
+type MyFloat float64
+
+func (v MyFloat) plusPlus(to MyFloat) MyFloat {
+	v += to // haha doesn't change initial value
+	return v
+}
+
+func (v *MyFloat) incInitial(to MyFloat) {
+	*v += to
+}
+
+
+func incInitial(v *MyFloat, to MyFloat) {
+	*v += to
+}
+
 func (v XYVertex) Abs() int {
 	return v.x*v.x + v.y*v.y
 }
 
+func Abs(v XYVertex) int {
+	return v.x*v.x + v.y*v.y
+} // same as above
+
+
+func testMethds() {
+	var v XYVertex = XYVertex{2, 3}
+	f := MyFloat(10)
+	incInitial(&f, MyFloat(44))
+	d := f.plusPlus(MyFloat(5))
+	f.incInitial(MyFloat(9))
+	fmt.Println(v.Abs(), Abs(v), d, f)
+}
+
 func main() {
-	var v XYVertex = XYVertex{2,3}
-	fmt.Println(v.Abs())
+	testMethds()
 }
